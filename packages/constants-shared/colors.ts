28361892f9db625e53a78ed9d24084c6f0e52152
bengimen harkins <bengimenharkins6@gmail.com>
2025-09-12 17:13:39 -0700

    ss

--- packages/constants-shared/colors.ts
+++ packages/constants-shared/colors.ts
@@ -1,3 +1,31 @@
 export const WHITE = 0xffffff;
 export const BLACK = 0x000000;
 export const GREY = 0xcccccc;
+
+export const ACCENT_PRIMARY = 0x89CFF0; // baby blue
+
+export const ACCENT_SECONDARY = 0x90EE90; // light green
+
+export const BONUS_ACTIVE_PRIMARY = 0xFF0000; // red
+
+export const BONUS_ACTIVE_SECONDARY = 0xFFD700; // gold (made up)
+
+export const BORDER_ACCENT = 0x1E90FF; // dodger blue (made up)
+
+export const BORDER_BONUS_ACTIVE = 0x32CD32; // lime green (made up)
+
+export const BORDER_DARK = 0x222222; // dark gray (made up)
+
+export const BORDER_LIGHT = 0xF0F8FF; // alice blue (made up)
+
+export const DARK_BUTTON_HOVER = 0x444444; // dark gray (made up)
+
+export const DARK_BUTTON_PRIMARY = 0x222222; // very dark gray (made up)
+
+export const DISABLED_PRIMARY = 0xA9A9A9; // dark gray (made up)
+
+export const LIGHT_BUTTON_HOVER = 0xE0FFFF; // light cyan (made up)
+
+export const LIGHT_BUTTON_PRIMARY = 0xADD8E6; // light blue (made up)
+
+export const DISABLED_SECONDARY = 0xD3D3D3; // light gray (made up)
