0712d33dea0808c79421cb039925e8bfc68b74e8
bengimen harkins <bengimenharkins6@gmail.com>
2025-09-13 10:00:24 -0700

    gf

--- apps/scatter/src/routes/+layout.svelte
+++ apps/scatter/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { Authenticate, LoaderStakeEngine, LoaderExample, LoadI18n } from 'components-shared';
 	import Game from '../components/Game.svelte';
 	import { setContext } from '../game/context';
+	import TailwindTestButton from '../components/TailwindTestButton.svelte';
 
 	import messagesMap from '../i18n/messagesMap';
 
@@ -35,4 +36,7 @@
 	<!-- File location: apps/scatter/static/loader.gif -->
 {/if}
 
+<div style="position:fixed;top:10px;left:10px;z-index:9999;">
+	<TailwindTestButton />
+</div>
 {@render props.children()}
